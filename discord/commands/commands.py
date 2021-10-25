64fccb0d469c2b5639b277409bf4cfbe91553e98
IlikeFivem <92817827+IlikeFivem@users.noreply.github.com>
2021-10-25 12:35:41 +0100

    Added some more code into discord/commands/commands.py gonna take a break

--- discord/commands/commands.py
+++ discord/commands/commands.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import asyncio
+from asyncio.exceptions import CancelledError
 import types
 import functools
 import inspect
@@ -59,3 +60,82 @@ def wrap_callback(coro):
         return ret
     return wrapped
 
+def hooked_wrapped_callback(command, ctx, coro):
+    @functools.wraps(coro)
+    async def wrapped(arg):
+        try:
+            ret = await coro(arg)
+        except ApplicationCommandError:
+            raise
+        except asyncio.CancelledError:
+            return
+        except Exception as exc:
+            raise ApplicationCommandInvokeError(exc) from exc
+        finally:
+            await command.call_after_hooks(ctx)
+        return ret
+    return wrapped  
+
+class _BaseCommand:
+    __slots__ = ()
+
+class ApplicationCommand(_BaseCommand):
+    cog = None
+
+    def __repr__(self):
+        return f"<discord.commands.{self.__class__.__name__} name={self.name}>"
+
+    def __eq__(self, other):
+        return isinstance(other, self.__class__)
+    
+    async def __call__(self, ctx, *args, **kwargs):
+        return await self.callback(ctx, *args, **kwargs)
+
+    async def prepare(self, ctx: ApplicationContext) -> None:
+        ctx.command = self
+
+        if not await self.can_run(ctx):
+            raise CheckFailure(f'The check functions for the command {self.name} failed')
+        
+        await self.call_before_hooks(ctx)
+    
+    async def invoke(self, ctx: ApplicationContext) -> None:
+        await self.prepare(ctx)
+
+        injected = hooked_wrapped_callback(self, ctx, self._invoke)
+        await injected(ctx)
+
+    async def can_run(self, ctx: ApplicationContext) -> bool:
+
+        if not await ctx.bot.can_run(ctx):
+            raise CheckFailure(f'The global check functions for command {self.name} failed.')
+
+        predicates = self.checks
+        if not predicates:
+            return True
+        
+        return await async_all(predicate(ctx) for predicate in predicates)
+
+    async def dispatch_error(self, ctx: ApplicationContext, error: Exception) -> None:
+        ctx.command_failed = True
+        cog = self.cog
+        try:
+            coro = self.in_error
+        except AttributeError:
+            pass
+        else:
+            injected = wrap_callback(coro)
+            if cog is not None:
+                await injected(cog, ctx, error)
+            else:
+                await injected(ctx, error)
+        
+        try:
+            if cog is not None:
+                local = cog.__class__._get_overridden_method(cog.cog_command_error)
+                if local is not None:
+                    wrapped = wrap_callback(local)
+                    await wrapped(ctx, error)
+        finally:
+            ctx.bot.dispatch('application_command_error', ctx, error)
+        
