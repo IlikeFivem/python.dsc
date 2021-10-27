198cda9f0fb25132630321fab1b19dccc29fbf03
IlikeFivem <92817827+IlikeFivem@users.noreply.github.com>
2021-10-27 13:55:19 +0100

    Added some code in discord\bot.py

--- discord/bot.py
+++ discord/bot.py
@@ -5,6 +5,39 @@ import collections
 import inspect
 import traceback
 from .commands.errors import CheckFailure
-from typing import List, Optional, Union
+
+from typing import (
+    Any,
+    Callable,
+    Coroutine,
+    List,
+    Optional,
+    TypeVar,
+    Union,
+)
 
 import sys
+
+from .client import Client
+from .shard import AutoShardedClient
+from .utils import MISSING, get, async_all
+from .commands import (
+    SlashCommand,
+    SlashCommandGroup,
+    MessageCommand,
+    UserCommand,
+    ApplicationCommand,
+    ApplicationContext,
+    command,
+)
+#from .cog import CogMixin
+
+from .errors import Forbidden, DiscordException
+from .interactions import Interaction
+
+CoroFunc = Callable[..., Coroutine[Any, Any, Any]]
+CFT = TypeVar('CFT', bound=CoroFunc)
+
+class ApplicationCommandMixin:
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
